058f200f1a2a9fb36bfd31f0d8f6ea0fd252810c
john <johnbaek.bjy@gmail.com>
2025-06-20 14:17:03 +0900

    Updates

--- src/JohnIsDev.Core/Features/Aop/RemoveLeadingNewLineAspect.cs
+++ src/JohnIsDev.Core/Features/Aop/RemoveLeadingNewLineAspect.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 namespace JohnIsDev.Core.Features.Aop;
 
 /// <summary>
-/// Remove \n 
+/// Remove
 /// </summary>
 public class RemoveLeadingNewLineAspect : AbstractInterceptorAttribute
 {
