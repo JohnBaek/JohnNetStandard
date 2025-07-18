985b9ab2a4f98092c6386d4ffcd6bf1d13a3d1a4
john <johnbaek.bjy@gmail.com>
2025-07-18 11:08:15 +0900

    Add unit test project for Entity Framework encryption library
    Created `JohnIsDev.Core.EntityFramework.Encryption.Test` project with initial test setup. Added basic NUnit configuration and referenced required packages. Updated solution to include the new test project.

---
+++ tests/JohnIsDev.Core.EntityFramework.Encryption.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace JohnIsDev.Core.EntityFramework.Encryption.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
